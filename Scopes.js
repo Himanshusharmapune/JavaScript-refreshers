11371e959d1c06398907cffa7887b788d15314e4
Himanshu sharma <himanshusharmapune@gmail.com>
2025-10-02 08:36:03 +0530

    Initial Commmit

---
+++ Scopes.js
@@ -0,0 +1,15 @@
+
+if (true) {
+  let a = 10
+  const b = 20
+  var c = 30
+  console.log("inner value", a);
+
+}
+let a = 50;
+console.log(a);
+
+// console.log(b);
+console.log(c);
+
+// curly braces are scopes {}
---
+++ functions.js
@@ -0,0 +1,48 @@
+// function addTwoNumbers (number1,number2){
+//     return (number1 + number2)
+ 
+// }
+// const a = addTwoNumbers(3,4)
+// console.log("result: ", a);
+
+// function loginUserMessage(username = "user"){
+//     if(!username){
+//         console.log("Please enter username")
+//         return
+//     }
+//     return `${username} just logged in!`
+// }
+
+// console.log(loginUserMessage())
+
+
+// function CalculateCartPrice(...num1){
+//     return num1;
+// }
+
+// console.log(CalculateCartPrice(10,20,30,40,50))
+
+
+// const user = {
+//     username:"Himanshu",
+//     age: 23
+// }
+
+// function HandleUser(anyobject){
+//     console.log(`The username is ${anyobject.username} and the user age is ${anyobject.age}` );
+// }
+
+// HandleUser({
+//      username:"Himanshu",
+//     age: 23
+// })
+
+
+const NewArray = [10,20,30,40]
+
+function returnSecondValue(Array){
+    return Array
+}
+
+console.log(returnSecondValue(NewArray))
+
