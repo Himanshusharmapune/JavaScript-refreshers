6c9f7e894d64233eb486402c97e671df1a66d90f
Himanshu sharma <himanshusharmapune@gmail.com>
2025-10-04 20:23:44 +0530

    Add this.js from main

---
+++ this.js
@@ -0,0 +1,17 @@
+//This keyword is used to refer to the current object.
+const user = {
+    username:"Himanshu",
+    age: 23,
+
+    WelcomeMessage: function(){
+        console.log(`${this.username} , welcome to the website` );
+        
+    }
+}
+
+user.WelcomeMessage()
+
+// user.WelcomeMessage({
+//      username:"Himanshu",
+//     age: 23
+// })
