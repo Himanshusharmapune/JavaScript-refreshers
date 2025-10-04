5d1e768f015d127e0bba2ba07c0651241aa7e54f
Himanshu sharma <himanshusharmapune@gmail.com>
2025-10-04 20:07:41 +0530

    Update project

--- Scopes.js
+++ Scopes.js
@@ -1,10 +1,8 @@
-
 if (true) {
-  let a = 10
-  const b = 20
-  var c = 30
+  let a = 10;
+  const b = 20;
+  var c = 30;
   console.log("inner value", a);
-
 }
 let a = 50;
 console.log(a);
@@ -14,4 +12,43 @@ console.log(c);
 
 // curly braces are scopes {}
 
+// Nested scope
+
+function one() {
+  const username = "Himanshu";
+
+  function two() {
+    const Lastname = "Sharma";
+    console.log(username);
+  }
+
+  two();
+}
+
+one();
+
+//Nested scope 2
+if (true) {
+  const username = "Carry";
+  if (username === "Carry") {
+    const Lastname = "minati";
+    console.log(username + " " + Lastname);
+  }
+//   Console.log(Lastname); // error
+}
+// console.log(username); // error
+
+
+// Hoisting
+
+console.log(Try(10)); // works
+function Try(num){
+    return num + 10;
+}
+
+
+console.log(Try2(10)); // error
+const Try2 = function(num){ // does not work & this is called function expression
+    return num + 20;
+}
 
