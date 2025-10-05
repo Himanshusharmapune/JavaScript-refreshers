203226e3bb1880d37dd9019cd129346ad5f65bee
Himanshu sharma <himanshusharmapune@gmail.com>
2025-10-05 17:32:55 +0530

    Add this.js

--- arrow.js
+++ arrow.js
@@ -1,1 +1,25 @@
-// git add this.js; git commit -m "Add this.js"; git push origin master
+//  const chai = () =>{
+//     console.log("Chai");
+//  }
+
+//  chai();
+
+// const addTwo = (a,b) => {
+//     return a + b;
+// }
+
+// console.log(addTwo(2,3));
+
+
+//implic return
+//eg1
+// const addTwo = (a,b) => a + b;
+// console.log(addTwo(3,3));
+
+//eg2 here you can add () if you want to avoid confusion
+// const addTwo = (a,b) => (a + b)
+// console.log(addTwo(3,3));
+
+//eg3 to return object with implicit return you have to use ()
+// const user = (username, age) => ({username: username, age: age});
+// console.log(user("Himanshu", 23));
