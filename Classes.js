0582801b1bdc99f4fa1d8b2c97d21e7a6ec63724
Himanshu sharma <himanshusharmapune@gmail.com>
2025-10-08 22:25:49 +0530

    Add Object.js

--- Classes.js
+++ Classes.js
@@ -1,11 +1,42 @@
 //declaring a class
-class Product{
+// class Product{
+//     constructor(ItemName, price,discount,productCode){
+//         this.ItemName = ItemName;
+//         this.price = price;
+//         this.discount = discount;
+//         this.productCode = productCode;
+//     }
+// }
+// let pencil = new Product('Pencil', 10, 2, 'P10');
+// console.log(pencil);
+
+//class expression
+// const Product1 = class Product{
+//     constructor(ItemName, price,discount,productCode){
+//         this.ItemName = ItemName;
+//         this.price = price;
+//         this.discount = discount;
+//         this.productCode = productCode;
+//     }
+// }
+// console.log(Product1.name);
+
+//Getter and Setter
+const Product1 = class Product{
     constructor(ItemName, price,discount,productCode){
         this.ItemName = ItemName;
         this.price = price;
         this.discount = discount;
         this.productCode = productCode;
     }
+    get getDsicountValue(){
+        return this.discount
+    }
+    set setDiscountValue(value){
+        this.discount = value;
+    }
 }
-let pencil = new Product('Pencil', 10, 2, 'P10');
-console.log(pencil);
+let chair = new Product1('Chair', 1000, 100, 'C100');
+console.log(chair.getDsicountValue);
+chair.setDiscountValue = 150;
+console.log(chair.getDsicountValue);
