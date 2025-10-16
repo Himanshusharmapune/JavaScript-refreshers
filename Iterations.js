5dc7d66637c2ac5983e9b1c0116dfc3fb3346636
Himanshu sharma <himanshusharmapune@gmail.com>
2025-10-16 23:22:51 +0530

    Add Iterations.js

---
+++ Iterations.js
@@ -0,0 +1,39 @@
+//an expample of for loop
+// for(i=0; i<=10; i++){
+//     const element = i;
+//     if(i===5){
+//         console.log('five');
+//     }
+//     else console.log(element);
+// }
+
+// for(i=1;i<=10;i++){
+//     console.log(`Outer loop is ${i}`);
+//     for(j=1;j<=i;j++){
+//         console.log(`${i} * ${j} = ${i*j}`);
+//     }
+// }
+
+//Array loop
+// let arr = [10,20,30,40,50];
+// for(let i=0;i<arr.length;i++){
+//     console.log(arr[i]);
+// }
+
+// Break 
+// for(let index = 1; index<=20; index++){
+//     if(index==10){
+//         console.log("Ten is found");    
+//         break;
+// }
+//     console.log(index);
+// }
+
+// Continue
+for(let index = 1; index<=20; index++){
+    if(index==10){
+        console.log("Ten is found");    
+        continue;
+}
+    console.log(index);
+}
