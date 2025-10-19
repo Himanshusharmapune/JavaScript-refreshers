844c73e14a11043571da1dfc1738dfc5a64bf776
Himanshu sharma <himanshusharmapune@gmail.com>
2025-10-19 19:54:13 +0530

    Add Iterations.js

--- Iterations.js
+++ Iterations.js
@@ -37,3 +37,53 @@
 // }
 //     console.log(index);
 // }
+
+//While loop
+// let index = 0;
+// while(index<=10){
+//     console.log(`value of index is ${index}`);
+//     index = index+2;
+// }
+
+//For off loop
+// const arr = [100,200,300,400,500];
+// for(const element of arr){
+//     console.log(element);
+// } 
+//here element takes value of each item in array one by one.
+
+// const arr = "Hello World";
+// for(const element of arr){
+//     console.log(element);
+// } 
+
+//Maps
+const map = new Map();
+map.set('name','John');
+map.set('age',30);
+map.set('city','New York');
+
+//console.log(map); //prints whole map
+
+//console.log(map.get('name')); //.get gets value of key passed in it
+//console.log(map.get('age'));
+//console.log(map.get('city'));
+
+//console.log(map.size); //.size gives number of key value pairs in map
+//console.log(map.has('name')); // .has checks if key is present in map or not and returns boolean value
+
+
+//now we are going to iterate through map using for of loop
+// for(const [key,value] of map){
+//     console.log(`${key} : ${value}`);
+// }
+
+//only for printing keys
+for(const [key,value] of map){
+    console.log(`${key}`);
+}
+
+//only for printing values
+for(const [key,value] of map){
+    console.log(`${value}`);
+}
