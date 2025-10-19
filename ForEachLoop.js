eaabf3ce084876f8409ba8684494cb091d985027
Himanshu sharma <himanshusharmapune@gmail.com>
2025-10-19 22:35:33 +0530

    Add ForEachLoop.js

---
+++ ForEachLoop.js
@@ -0,0 +1,20 @@
+// const a = [1,2,3,4,5]
+// a.forEach(function(b){
+//     console.log(b)
+// });
+
+//you can also define the function separately and then pass it to forEach
+// function printMe(item){
+//     console.log(item)
+// }
+// a.forEach(printMe);
+
+// a.forEach((item, index, arr)=> {
+//     console.log(item, index, arr); //here item is the current element, index is the index of current element and arr is the whole array
+// });
+
+//here we have used objects in an array and then used forEach to print a specific property of each object
+// const arr=[{name:'himanshu', age:24}, {name:'john', age:30}, {name:'doe', age:22}];
+// arr.forEach((item)=>{
+//     console.log(item.name)
+// })
