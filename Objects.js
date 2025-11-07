9001a4309c3a9ec9a7afdcdede6b3a9eaf1fd49c
Himanshu sharma <himanshusharmapune@gmail.com>
2025-11-07 22:48:33 +0530

    Objects

---
+++ Objects.js
@@ -0,0 +1,11 @@
+const user ={
+    username: "himanshu",
+    surname: "sharma",
+
+    printMessage: function(){
+        console.log("How are you?")
+    }
+}
+
+console.log(user.username);
+console.log(user.printMessage());
