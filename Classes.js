d2236606b2f3b9634658875578ae4299818536c5
Himanshu sharma <himanshusharmapune@gmail.com>
2025-11-10 08:44:06 +0530

    Classes.js

--- Classes.js
+++ Classes.js
@@ -40,3 +40,23 @@
 // console.log(chair.getDsicountValue);
 // chair.setDiscountValue = 150;
 // console.log(chair.getDsicountValue);      //
+
+
+class User {
+    constructor(username, email, password){
+        this.username = username;
+        this.email = email;
+        this.password = password
+    }
+    encryptPassword(){
+        return `${this.password}abc`
+    }
+    changeUsername(){
+        return `${this.username.toUpperCase()}`
+    }
+}
+
+const chai = new User("chai", "chai@gmail.com", "123")
+console.log(chai.encryptPassword());
+console.log(chai.changeUsername());
+
