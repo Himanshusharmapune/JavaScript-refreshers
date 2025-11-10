700e35dd746bbc5832c9073b468998cfdd2ae2ef
Himanshu sharma <himanshusharmapune@gmail.com>
2025-11-10 09:49:17 +0530

    staticprop.js

---
+++ staticprop.js
@@ -0,0 +1,24 @@
+class User{
+    constructor(username,id){
+        this.username = username;
+        this.id = id;
+    }
+    logMe(){
+        console.log(`username: ${this.username}`);
+    }
+   static createId(){
+       console.log(`id: ${this.id}`)
+    }
+}
+class Teacher extends User{
+    constructor(username, email, id){
+        super(username,id)
+        this.email = email
+       
+        
+    }
+}
+const Teacherid = new Teacher("Himanshu","himanshu@gmail.com", "111")
+// const himanshu = new  User("Himanshu");
+// console.log(himanshu.createId())
+console.log(Teacherid.createId())
